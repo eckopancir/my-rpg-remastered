f5559cd22113694ac3ff23ba2376828c0c073629
Sviridov Alexsey Andreevich <eckopancir@gmail.com>
2026-07-27 20:11:35 +0500

    chore: bump to rewards-v3.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-const BUILD_VERSION = '2026-07-27 rewards-v3.2';
+const BUILD_VERSION = '2026-07-27 rewards-v3.3';
 console.log('BUILD VERSION:', BUILD_VERSION);
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
