f6ee8a5baf48aef140ce14a8c23974cb2818e621
Sviridov Alexsey Andreevich <eckopancir@gmail.com>
2026-09-25 07:12:55 +0500

    \customization: nudge firearm mod slots (magazine under receiver etc)"

--- src/components/widgets/CustomizationModal.tsx
+++ src/components/widgets/CustomizationModal.tsx
@@ -25,11 +25,11 @@ const SCALE = 0.78;
 
 const FIREARM_SLOT_POSITIONS: ModSlotPos[] = [
   { id: 'mod_scope', name: 'Прицел', top: Math.round(93 * SCALE), left: Math.round(193 * SCALE) },
-  { id: 'mod_barrel', name: 'Ствол', top: Math.round(130 * SCALE), left: Math.round(340 * SCALE) },
+  { id: 'mod_barrel', name: 'Ствол', top: Math.round(142 * SCALE), left: Math.round(346 * SCALE) },
   { id: 'mod_receiver', name: 'Ресивер', top: Math.round(170 * SCALE), left: Math.round(240 * SCALE) },
-  { id: 'mod_muzzle', name: 'Дуло', top: Math.round(113 * SCALE), left: Math.round(439 * SCALE) },
-  { id: 'mod_magazine', name: 'Магазин', top: Math.round(250 * SCALE), left: Math.round(130 * SCALE) },
-  { id: 'mod_stock', name: 'Приклад', top: Math.round(168 * SCALE), left: Math.round(130 * SCALE) },
+  { id: 'mod_muzzle', name: 'Дуло', top: Math.round(132 * SCALE), left: Math.round(451 * SCALE) },
+  { id: 'mod_magazine', name: 'Магазин', top: Math.round(283 * SCALE), left: Math.round(240 * SCALE) },
+  { id: 'mod_stock', name: 'Приклад', top: Math.round(168 * SCALE), left: Math.round(117 * SCALE) },
 ];
 
 const COLD_WEAPON_SLOT_POSITIONS: ModSlotPos[] = [
